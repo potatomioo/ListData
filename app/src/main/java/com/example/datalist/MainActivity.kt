bdad0f562664f7ec04a5ce798699f479a2485951
Deeptanshu Shukla <108982699+x-deeptanshu@users.noreply.github.com>
2024-04-15 17:47:09 +0530

    Second

--- app/src/main/java/com/example/datalist/MainActivity.kt
+++ app/src/main/java/com/example/datalist/MainActivity.kt
@@ -3,44 +3,138 @@ package com.example.datalist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.datalist.ui.theme.DataListTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DataListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            input()
+        }
+    }
+}
+
+    
+@Composable
+fun input() {
+    var name by remember {
+        mutableStateOf("")
+    }
+    var names by remember {
+        mutableStateOf(listOf<String>())
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(
+                    color = Color.White,
+                    RoundedCornerShape(bottomStart = 25.dp, bottomEnd = 25.dp)
+                )
+        ) {
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(15.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+
+
+                    
+
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = name,
+                        onValueChange = { content ->
+                            name = content
+                        },
+                        label = {
+                            Text(text = "Name")
+                        },
+                        supportingText = {
+                            Text(text = "${name.length}/20")
+                        },
+                        singleLine = true
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Button(
+                        onClick = {
+                            if (name.isNotBlank()) {
+                                names = names + name
+                            }
+                        }
+
+                    ) {
+                        Text(text = "Add")
+                    }
+                    
+
+
+                }
+
+
+
+            
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .align(Alignment.BottomCenter)
+                .background(
+                    color = Color.White,
+                )
+        ) {
+            LazyColumn {
+                items(names){
+                        cout->
+                    Text(text = cout,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth()
+                    )
+                    Divider()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DataListTheme {
-        Greeting("Android")
-    }
+private fun hey() {
+    input()
 }
